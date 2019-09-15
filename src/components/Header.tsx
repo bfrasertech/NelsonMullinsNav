8c45603e6cccb9578567dbeafd802113e48c3e93
Brian Fraser <bfraser@frasertechnicalsolutions.com>
2019-09-15 17:19:43 -0400

    Added alert and top menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,72 @@
 import * as React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import {faSearch, faTimesCircle} from '@fortawesome/free-solid-svg-icons';
 
 import '../styles/style.css';
 
 export interface IHeaderProps {}
-export interface IHeaderState {}
+export interface IHeaderState {
+    showAlert: boolean;
+}
 
 /* tslint:disable no-any */
 const nmLogo: any = require('../images/nm_logo.png');
 
-export default class Header extends React.Component<IHeaderProps> {
+export default class Header extends React.Component<IHeaderProps, IHeaderState> {
     constructor(props: IHeaderProps) {
 
         super(props);
+
+        this.state = {
+            showAlert: true
+        };
+
+        document.addEventListener('keypress', this.handleKeyPress, false);
+    }
+
+    private handleKeyPress = (event: any): void => {
+        if (event.keyCode === 49){
+            console.log('f1');
+            this.setState({
+                showAlert: !this.state.showAlert
+            });
+        }
+
+        if (event.keyCode === 50){
+            console.log('f2');
+        }
     }
 
     public render(): React.ReactElement<IHeaderProps> {
-        return (<div className='nmrs-nav-header-container'>
-            <div className='nmrs-nav-alert-container'></div>
+        return (
+        <div>
+            { this.state.showAlert && 
+            <div className='nmrs-nav-alert-container'>
+                <div className='nmrs-nav-alert-message'><span>Alert: System will be down for maintenance Saturday 3/14/19 from 7 PM to 9 PM EST</span>
+                </div>
+                <button type='button' className='nmrs-nav-alert-close-button' onClick={() => this.setState({showAlert: false})}><FontAwesomeIcon icon={faTimesCircle} /></button>
+            </div>}
+            <div className='nmrs-nav-header-container'>
             <img src={nmLogo} alt='Logo' />
             <div className='nmrs-nav-search-container'>
                 <input type="text" className='nmrs-nav-search-box' value="Search for People, Clients, Matters and Internet Content here…"></input>
                 <button type='button' className='nmrs-nav-advanced-search-button'>Advanced Search</button>
                 <button type='button' className='nmrs-nav-search-button'><FontAwesomeIcon icon={faSearch} /></button>
             </div>
-        </div>);
+        </div>
+        <div className='nmrs-nav-menu-container'>
+            <ul>
+                <li>Home</li>
+                <li>Firm</li>
+                <li>Management Groups</li>
+                <li>Teams</li>
+                <li>Committees</li>
+                <li>Offices</li>
+                <li>Administration</li>
+                <li>How Do I?</li>
+            </ul>
+        </div>
+        </div>
+        );
     }
 }
