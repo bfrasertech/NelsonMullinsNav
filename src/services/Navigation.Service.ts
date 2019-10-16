35ceca2efc5125acfe4de5dbe6cc2becc8a288be
Brian Fraser <BXFRASER@nelsonmullins.com>
2019-10-15 20:07:43 -0400

    Removed old SP fetch teams method

--- src/services/Navigation.Service.ts
+++ src/services/Navigation.Service.ts
@@ -11,41 +11,8 @@ export interface ITeamItem{
     Title: string;
 }
 
-
-
 const teamsList = '176799b5-1e0b-42bc-9e11-778f85851935';
 const mapSPResultToTeam = (spResult: ITeamItem): ITeam => ({ id: spResult.Id, title: spResult.Title });
-
-export const fetchTeamsx = (context: ApplicationCustomizerContext): Promise<ITeam[]> => {
-
-    let listUrl = `${
-
-        context.pageContext.site.absoluteUrl
-        }/_api/web/lists('${teamsList}')/items`;
-
-
-    return new Promise<ITeam[]>((resolve: (teams: ITeam[]) => void, reject: (error: any) => void): void => { 
-
-        context.spHttpClient
-            .get(listUrl, SPHttpClient.configurations.v1)
-            .then((response: SPHttpClientResponse): Promise<ITeamItem[]> => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    reject(response);
-                    // reject (response.statusText);
-                }
-            })
-            .then((teamItems: any): void => {
-                resolve(teamItems.value.map((item: ITeamItem): ITeam => mapSPResultToTeam(item)));
-            })
-            .catch((error: any): void => {
-                console.log('Error getting teams');
-                reject(error);
-            });
-
-    });
-}
 
 export interface IOffice {
     id: string;
