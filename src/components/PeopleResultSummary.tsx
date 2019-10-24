24ae2ed36bca8a782b6fd90ad87c2c3118ff4ad2
Brian Fraser <BXFRASER@nelsonmullins.com>
2019-10-23 22:32:26 -0400

    Updated people search styling

--- src/components/PeopleResultSummary.tsx
+++ src/components/PeopleResultSummary.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faExternalLink} from '@fortawesome/pro-solid-svg-icons';
+import { faExternalLink, faInfoCircle } from '@fortawesome/pro-solid-svg-icons';
 
 
 import * as peopleSearchServices from '../services/PeopleSearch.Service';
@@ -10,23 +10,23 @@ import { PersonCard } from './PersonCard';
 import classes from './PeopleResultSummary.module.scss';
 
 export interface IPeopleResultSummaryProps {
-    peopleResults: peopleSearchServices.IPerson[];
+  peopleResults: peopleSearchServices.IPerson[];
 }
 
 export const PeopleResultsSummary = (props: IPeopleResultSummaryProps): React.ReactElement<IPeopleResultSummaryProps> => {
-    return (
-        <div className={classes.container}>
-          <div className={classes.header}>
-            <div>People Directory</div> 
-            <div>
-              <span>More</span> <FontAwesomeIcon icon={faExternalLink} />
-            </div>
-          </div>
+  return (
+    <div className={classes.container}>
+      <div className={classes.header}>
+        <div>People Directory<FontAwesomeIcon icon={faInfoCircle} /></div>
+        <div>
+          <span className={classes.moreText}>More</span> <FontAwesomeIcon className={classes.moreIcon} icon={faExternalLink} />
+        </div>
+      </div>
 
-          {props.peopleResults.map((person: peopleSearchServices.IPerson) => {
-            return (
-              <PersonCard person={person} />
-            );
-          })}
-        </div>);
+      {props.peopleResults.map((person: peopleSearchServices.IPerson) => {
+        return (
+          <PersonCard person={person} />
+        );
+      })}
+    </div>);
 }
